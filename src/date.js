f51fea62d2a19e6a73769866dd0ec80e544eaf1e
Sandra Tena <sandratena06@gmail.com>
2022-06-29 14:49:31 -0700

    debug results display
    Co-authored-by: Mo Byrne <mosey.b.byrne@gmail.com>

---
+++ src/date.js
@@ -0,0 +1,14 @@
+export default class Holiday {
+  static getHoliday(date) {
+    return fetch(`https://holidays.abstractapi.com/v1/?api_key=${process.env.API_KEY}&country=US&year=2022&month=${date[1]}&day=${date[2]}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });        
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Holiday from './date';
+
+function clearFields() {
+  $('.date').val("");
+  $('.showNames').append("");
+  $('.showCountry').text("");
+  $('.showDescription').text("");
+}
+
+function getElements(response) {
+  console.log(response);
+  console.log(`${response.name}`);
+  for (let i = 0; i <= 4; i ++) {
+    console.log(i);
+    if (response[i]) {
+      $('.showName').append("<p>" + `Holiday Name: ${response[i].name}` 
+       + "<br>" + `Holiday Type: ${response[i].type}` + "<br>" + `Country: ${response[i].country}` + "</p>");
+    }
+  }
+}
+
 
 $(document).ready(function () {
-  $('selector').submit(function (event)) {
-    event.preventDefault(); //if it's a form
-    // UI code here
-  }
+  $('#selector').submit(function(event) {
+    event.preventDefault();
+    const inputDate = $("input#date").val();
+    let date = inputDate.split('-');
+    clearFields();
+    Holiday.getHoliday(date)
+      .then(function(response) {
+        getElements(response);
+      });
+  });
 });
